f27549f506d31b95a86f4d60a4244d5bd107b1e0
Carol <carolqueiroz96@outlook.com.br>
2022-06-06 21:35:28 -0300

    primeiro commit

---
+++ src/Atividade1/Funcionarios.java
@@ -0,0 +1,69 @@
+package Atividade1;
+
+public class Funcionarios {
+    public String nome;
+    public Integer idade;
+    public String cargo;
+    public String situacao;
+
+    public Funcionarios(String nome, Integer idade, String cargo, String situacao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cargo = cargo;
+        this.situacao = situacao;
+    }
+
+    public void demitirFuncionario(){
+        System.out.println("A funcionária " + getNome() + " será demitida.");
+    }
+
+    public void trocarCargoFuncionario(){
+        System.out.println("A funcionária " + getNome() + " foi promovida de " + getCargo() + " para Analista Sênior.");
+
+    }
+
+    public void pagarSalario(){
+        System.out.println("A funcionária " + getNome() + " deve receber o seu salário, no valor de " + "R$2.500,00.");
+
+    }
+
+    public void fazerAniversario(){
+        idade++;
+        System.out.println("A funcionária " + getNome() + " está fazendo " +idade+ " anos, Parabéns!");
+
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+}
---
+++ src/Atividade1/Principal.java
@@ -0,0 +1,18 @@
+package Atividade1;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Funcionarios f1 = new Funcionarios("Carol", 25, "Analista Junior", "inativo");
+        Funcionarios f2 = new Funcionarios("Jociane", 27, "Analista Pleno", "ativo");
+        Funcionarios f3 = new Funcionarios("Amanda", 28, "Analista Sênior", "ativo");
+        Funcionarios f4 = new Funcionarios("Débora", 30, "Analista Juior", "inativo");
+
+        f1.demitirFuncionario();
+        f2.trocarCargoFuncionario();
+        f4.pagarSalario();
+        f3.fazerAniversario();
+    }
+
+
+}
